b564a2fff43ca0399178de28aa33175877e75753
Khoa Le <le.khoa2209@gmail.com>
2023-08-04 18:25:18 +0700

    Feat: Implement the search filters string

--- packages/ui/src/components/SearchResults/SearchFilters/components/SearchLocationAutocomplete.tsx
+++ packages/ui/src/components/SearchResults/SearchFilters/components/SearchLocationAutocomplete.tsx
@@ -12,7 +12,7 @@ const SearchLocationAutocomplete: React.FC<{
   const icon = <CheckCircleIcon fontSize="small" color="disabled" />
   const checkedIcon = <CheckCircleIcon fontSize="small" />
 
-  const [selectLocations, setSelectLocations] = React.useState<GetListingsTypes.PopularLocation[]>([])
+  const [selectLocations, setSelectLocations] = React.useState<(string | GetListingsTypes.PopularLocation)[] | undefined>()
 
   const countCharacterOccurrences = (char: string, str?: string): number => {
     const regex = new RegExp(char, 'g')
@@ -21,6 +21,7 @@ const SearchLocationAutocomplete: React.FC<{
   }
 
   const toggleLocation = (option: GetListingsTypes.PopularLocation) => {
+    // @ts-ignore
     setSelectLocations(selectNextLocation(option, selectLocations, searchForm?.popular_locations))
   }
 
@@ -30,6 +31,10 @@ const SearchLocationAutocomplete: React.FC<{
     { selected }: { selected: boolean }
   ) => {
     const numberOfParents = countCharacterOccurrences(' > ', option.id)
+    // @ts-ignore
+    if (selectLocations && typeof selectLocations === 'string' && selectLocations.length > 1) {
+      return null
+    }
     return (
       <li {...props} key={option.slug + option.id} onClick={() => toggleLocation(option)}>
         <Checkbox icon={icon} checkedIcon={checkedIcon} style={{ marginRight: 8, paddingLeft: numberOfParents * 24 }} checked={selected} />
@@ -43,8 +48,10 @@ const SearchLocationAutocomplete: React.FC<{
       multiple
       limitTags={1}
       disableCloseOnSelect
+      freeSolo
       value={selectLocations}
       options={searchForm?.popular_locations}
+      // @ts-ignore
       getOptionLabel={(option) => option?.name || ''}
       renderOption={renderOption}
       renderTags={(tagValue, getTagProps) => {
@@ -53,7 +60,10 @@ const SearchLocationAutocomplete: React.FC<{
           <Chip {...getTagProps({ index })} label={option.name} onDelete={() => toggleLocation(option)} key={option.id} />
         ))
       }}
-      renderInput={(params) => <TextField {...params} placeholder={searchForm.texts.searchPlaceholder} />}
+      renderInput={(params) => (
+        // @ts-ignore
+        <TextField {...params} placeholder={searchForm.texts.searchPlaceholder} onChange={(e) => setSelectLocations(e.target.value)} />
+      )}
     />
   )
 }
